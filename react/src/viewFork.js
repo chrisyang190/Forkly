297ae6f35f17c09f1140c663b3df22346e54f5d0
RDF014 <rdf014@gmail.com>
2017-04-01 12:44:20 -0700

    created work around, but DDoSing myself

--- react/src/viewFork.js
+++ react/src/viewFork.js
@@ -8,11 +8,13 @@ class ViewFork extends React.Component {
   constructor(props) {
     super(props);
     this.forkMe = this.forkMe.bind(this);
+    this.goToFork = this.goToFork.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
   }
 
   componentDidMount(){
     console.log(this.props);
-    let pathname = this.context.router.route.location.pathname;
+    let pathname = this.props.location.pathname;
     let recipeId = pathname.slice(pathname.lastIndexOf('/') + 1);
     let checkPath = pathname.split('/')[1];
     let boundThis = this;
@@ -23,7 +25,32 @@ class ViewFork extends React.Component {
       data: {id: recipeId},
       success: (data)=>{
         console.log(data)
-        boundThis.setState({
+        this.setState({
+          recipe: data,
+          path: checkPath
+        });
+        console.log(this.state)
+      },
+      error: function(err) {
+        console.error('could not retrieve any recipes for user');
+      }
+    });
+  }
+
+  componentDidUpdate() {
+    console.log(this.props);
+    let pathname = this.props.location.pathname;
+    let recipeId = pathname.slice(pathname.lastIndexOf('/') + 1);
+    let checkPath = pathname.split('/')[1];
+    let boundThis = this;
+    console.log(recipeId)
+     $.ajax({
+      url: '/getRecipeById',
+      type:'GET',
+      data: {id: recipeId},
+      success: (data)=>{
+        console.log(data)
+        this.setState({
           recipe: data,
           path: checkPath
         });
@@ -42,6 +69,14 @@ class ViewFork extends React.Component {
     router.history.push('/test/' + forkedId);
   }
 
+  goToFork(){
+    console.log('clicked', this.props)
+    const { router } = this.context;
+    console.log(router.history.push)
+    router.history.push(`/recipe/${this.state.recipe.forked._id}`);
+    this.componentDidMount();
+  }
+
   render () {
     let template = '';
     if (this.state) {
@@ -50,7 +85,7 @@ class ViewFork extends React.Component {
       <div className="viewFork">
         <header>
           <h1 className="recipeName">{`${recipe._creator.name}'s ${recipe.name}`}</h1>
-          {this.state.recipe.forked ? <h4>{`Forked From ${this.state.recipe.forked._creator.name}'s ${this.state.recipe.forked.name}`}</h4> : null}
+          {this.state.recipe.forked ? <h4><a onClick={this.goToFork}>{`Forked From ${this.state.recipe.forked._creator.name}'s ${this.state.recipe.forked.name}`}</a></h4> : null}
           <h1 className="recipeName">Tags:</h1>
         {recipe.tags.map(( tag => <p>{tag}</p>))}
           <br />
